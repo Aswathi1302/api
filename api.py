1ec5f3b9ab61806fcba92802d6c96f0b694837e0
Aswathi1302 <aswathisomaraj1302@gmail.com>
2022-11-03 16:20:18 +0530

    first commit

--- api.py
+++ api.py
@@ -1,5 +1,10 @@
 import mysql.connector
-mydb= mysql.connector.connect(host= 'localhost',user='root',password='',database=' hoteldb')
+try:
+    mydb= mysql.connector.connect(host= 'localhost',user='root',password='',database=' userdb')
+except mysql.connector.Error as e:
+    #print("connection error")   
+    sys.exit("dbconnection failure")
+mycursor= mydb.cursor()
 import requests
 import json
 data=requests.get("https://jsonplaceholder.typicode.com/users").text
@@ -7,7 +12,11 @@ data=requests.get("https://jsonplaceholder.typicode.com/users").text
 data_info=json.loads(data)
 #print(data_info)
 
-user_list=[]
+#user_list=[]
 for i in data_info:
-    user_list.append([i["name"],i["email"],i["phone"]])
-print(user_list)    
+    #user_list.append([i["name"],i["email"],i["phone"]])
+    sql="INSERT INTO `usertbl`(`name`, `email`, `phone`) VALUES ('"+i['name']+"','"+i['email']+"','"+i['phone']+"')"
+    mycursor.execute(sql)
+    mydb.commit()
+    print("data inserted successfully....",i["name"])
+#print(user_list)    
